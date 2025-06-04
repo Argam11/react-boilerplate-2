1d6fe8d0de10b7df5dcd623a91c19d55411c2d5a
Argam <argam.markosyan1994@gmail.com>
2025-06-04 19:04:04 +0400

    setup

---
+++ src/tests/example.test.tsx
@@ -0,0 +1,13 @@
+import { describe, it, expect, vi, beforeEach } from 'vitest';
+
+describe('EditorTextElement', () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it('renders text with correct position (x, y coordinates)', () => {
+    expect(11).toBe(11);
+    expect(true).toBe(true);
+  });
+
+});
---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import path from "path";
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import type { ViteUserConfig } from "vitest/config";
+import { visualizer } from "rollup-plugin-visualizer";
+
+const vitestConfig: ViteUserConfig = {
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/tests/setup.ts",
+  },
+};
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [visualizer(), react()],
+  test: vitestConfig.test,
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@constants": path.resolve(__dirname, "src/constants"),
+      "@helpers": path.resolve(__dirname, "src/helpers"),
+    },
+  },
+});
